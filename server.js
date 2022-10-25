dba3188ab16219628d389df1f57fb2b08abbbc95
Angel Lammenda <angellammenda@hotmail.com>
2022-10-25 06:10:12 -0300

    tp4

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const express = require('express');
-require (`dotenv`).config();
-const app = express();
-const PORT = process.env.PORT || 3003;
+// const express = require('express');
+// require (`dotenv`).config();
+// const app = express();
+// const PORT = process.env.PORT || 3003;
 
-const Contenedor = require(`./tp2.js`);
-const contenedor = new Contenedor("productos.txt");
+// const Contenedor = require(`./tp2.js`);
+// const contenedor = new Contenedor("productos.txt");
 
-app.use (express.json());
-app.use (express.urlencoded({extended: true}));
+// app.use (express.json());
+// app.use (express.urlencoded({extended: true}));
 
-app.get (`/productos`, async (_req, res) => {
-    const productos = await contenedor.getAll();
-    res.status(200).json(productos);
-}
-);
+// app.get (`/productos`, async (_req, res) => {
+//     const productos = await contenedor.getAll();
+//     res.status(200).json(productos);
+// }
+// );
 
-app.get ("/productos-random", async (_req, res) => {
-    const productos = await contenedor.getAll();
-    let random = Math.floor(Math.random()*productos.length);
-    res.status(200).json(productos[random]);
-}
-)
-
-
+// app.get ("/productos-random", async (_req, res) => {
+//     const productos = await contenedor.getAll();
+//     let random = Math.floor(Math.random()*productos.length);
+//     res.status(200).json(productos[random]);
+// }
+// )
 
 
 
-app.listen (PORT, ()=> {
-    console.log (`Servidor escuchando en el puerto ${PORT}`);
-});
+
+
+// app.listen (PORT, ()=> {
+//     console.log (`Servidor escuchando en el puerto ${PORT}`);
+// });
